683cde196b6bc4bb190b2041d4789af1cd640b46
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-26 01:33:51 +0900

    initial commit

---
+++ src/MAML.py
@@ -0,0 +1,110 @@
+import torch.optim
+from torch import nn
+import torchvision as tv
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from src.MAMLDataset import MAMLEpisoder, MAMLDataset
+import torch.nn.functional as F
+
+def forward(x, params):
+  x = F.conv2d(x, params['conv1.weight'], bias=params['conv1.bias'], stride=1, padding=1)
+  x = F.relu(x)
+  x = F.conv2d(x, params['conv2.weight'], bias=params['conv2.bias'], stride=1, padding=1)
+  x = F.relu(x)
+  x = x.flatten()
+  x = F.linear(x, weight=params['l1.weight'], bias=params['l1.bias'])
+  x = F.relu(x)
+  return F.softmax(x)
+# forward()
+
+class MAMLNet(nn.Module):
+  def __init__(self, inpt_channels: int, hidn_channels: int, oupt_channels: int):
+    super(MAMLNet, self).__init__()
+    self.conv1 = nn.Conv2d(inpt_channels, hidn_channels, kernel_size=3, padding=1, stride=1)
+    self.conv2 = nn.Conv2d(hidn_channels, hidn_channels, kernel_size=3, padding=1, stride=1)
+    self.l1 = nn.Linear(in_features=150528, out_features=oupt_channels)
+    self.relu, self.flatten, self.softmax = nn.ReLU(), nn.Flatten(), nn.Softmax()
+  # __init__
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.relu(x)
+    x = self.conv2(x)
+    x = self.relu(x)
+    x = self.flatten(x)
+    x = self.l1(x)
+    x = self.relu(x)
+    return self.softmax(x)
+  # forward
+# MAMLNet
+
+class MAML:
+  def __init__(self, model_config: tuple, basic_config: tuple, meta_config: tuple, iters: int):
+    self.model = MAMLNet(*model_config).to(device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    self.lr, self.wd = basic_config
+    self.mt_lr, self.mt_wd = meta_config
+    self.iters = iters
+  # __init__()
+
+  def inner_update(self, task: MAMLDataset):
+    local_params = {name: param.clone() for name, param in self.model.named_parameters()}
+    for feature, label in DataLoader(task, shuffle=True):
+      pred = forward(feature, local_params)
+      loss = nn.MSELoss()(pred, label)
+      grads = torch.autograd.grad(loss, list(local_params.values()), create_graph=True)
+      local_params = {name: param - self.lr * grad for (name, param), grad in zip(local_params.items(), grads)}
+    return local_params
+  # inner_update()
+# MAML
+
+if __name__ == "__main__":
+  tv.datasets.Omniglot(root="./data/", background=True, download=True)  # download dataset
+  imageset = tv.datasets.ImageFolder(root="./data/omniglot-py/images_background/Futurama")  # load dataset
+
+  # define transform
+  transform = tv.transforms.Compose([
+    tv.transforms.Resize((224, 224)),
+    tv.transforms.ToTensor(),
+    tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+  ])  # transform
+
+  # 5 way 5 shot 설정을 적용합니다.
+  n_way, k_shot, n_query = 5, 5, 2  # define framework to use
+
+  # create FSL episode generator
+  chosen_classes = list(imageset.class_to_idx.values())[:n_way]
+  episoder = MAMLEpisoder(imageset, chosen_classes, k_shot, n_query, transform)
+
+  # init model
+  basic_config, meta_config = (0.001, 0.01), (0.001, 0.01)
+  model = MAML((3, 3, n_way), basic_config, meta_config, 5)
+  criterion = nn.MSELoss()
+  optim = torch.optim.Adam(model.model.parameters(), lr=0.0001, weight_decay=0.01)
+
+  # train loop
+  iters, whole_loss = 50, float()
+  for _ in tqdm(range(iters)):
+    tasks, query_set = episoder.get_episode()
+    fast_adaptions = list()
+    for task in tasks: fast_adaptions.append(model.inner_update(task))
+    loss = float()
+    for feature, label in DataLoader(query_set, shuffle=True):
+      pred = forward(feature, fast_adaptions[torch.argmax(label)])
+      loss += criterion(pred, label)
+    # for
+    loss /= n_way
+    whole_loss += loss.item()
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+    print(loss.item())
+  # for
+  print(whole_loss / iters)
+
+  _, query_set = episoder.get_episode()
+  cnt = 0
+  for feature, label in DataLoader(query_set, shuffle=True):
+    pred = model.model.forward(feature)
+    if torch.argmax(pred) == torch.argmax(label): cnt += 1
+  print(f"{cnt}/{len(query_set)}")
+# if
---
+++ src/MAMLDataset.py
@@ -0,0 +1,50 @@
+import random
+import typing
+import torch.nn.functional as F
+import torch
+from torch.utils.data import Dataset
+
+class MAMLDataset(Dataset):
+  def __init__(self, dataset, classes: list, indices_t: list, transform: typing.Callable):
+    self.dataset, self.indices_t, self.classes = dataset, indices_t, classes
+    self.transform = transform
+  # __init__()
+  def __len__(self): return len(self.indices_t)
+
+  def __getitem__(self, index: int):
+    """ Returns a sample from the dataset at the given index.
+      Args: index of the sample to be retrieved.
+      Returns: tuple of the transformed feature and the label. """
+    assert index < len(self.indices_t), IndexError("Index out of bounds") # check if index is out of bounds
+    feature, label = self.dataset[self.indices_t[index]]
+    # apply transformation
+    feature = self.transform(feature)
+    label = F.one_hot(torch.tensor(label), num_classes=len(self.classes)).float()
+    return feature, label
+  # __getitem__()
+# MAMLDataset
+
+class MAMLEpisoder:
+  def __init__(self, dataset, classes: list, k_shot: int, n_query: int, transform:typing.Callable):
+    assert k_shot > 0 or n_query > 0, ValueError("k_shot and n_query must be greater than 0.")
+    self.dataset, self.classes = dataset, classes
+    self.transform = transform
+    self.k_shot, self.n_query = k_shot, n_query
+    self.indices_t = self.get_task_indices()
+  # __init__()
+
+  def get_task_indices(self):
+    indices_t = {label: [] for label in self.classes}
+    for index, (_, label) in enumerate(self.dataset):
+      if label in self.classes: indices_t[label].append(index)
+    return indices_t
+  # get_task_indices
+
+  def get_episode(self):
+    tasks, query_set = list(), list()
+    for indices_t in self.indices_t.values():
+      indices_t = random.sample(indices_t, self.k_shot + self.n_query)
+      tasks.append(MAMLDataset(self.dataset, self.classes, indices_t[:self.k_shot], self.transform))
+      query_set.extend(indices_t[self.k_shot:])
+    return tasks, MAMLDataset(self.dataset, self.classes, query_set, self.transform)
+# MAMLDataset
