f6467d18ebca8cecfeadb933766f7881e478b99f
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-26 11:12:55 +0900

    feat: add evaluation script

--- src/eval.py
+++ src/eval.py
@@ -1,19 +1,25 @@
 import torch
-
+from torch.utils.data import DataLoader
 from src.model.MAML import MAML
 
-
-def main(model: str = "./model/model.pth"):
+def main(model: str):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
   # load model
   data = torch.load(model)
   state = data["state"]
-  transform = data["transform"]
-  model = MAML().to(device)
+  episoder = data["episoder"]
+  model = MAML(*data["model config"]).to(device)
   model.load_state_dict(state)
   model.eval()
 
-  # create FSL episode generator
-  imageset = tv.datasets.ImageFolder(root=path)
-  chosen_classes = list(imageset.class_to_idx.values())[:n_way]
+  tasks, query_set = episoder.get_episode()
+  cnt = 0
+  for feature, label in DataLoader(query_set, shuffle=True):
+    pred = model.forward(feature)
+    if torch.argmax(pred) == torch.argmax(label): cnt += 1
+  # for
+  print(f"accuracy: {cnt / len(query_set):.2f}({cnt}/{len(query_set)})")
+# main
+
+if __name__ == "__main__": main(model="./model/model.pth")
