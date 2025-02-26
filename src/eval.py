d568523efd73cd475319f0fe2d00f7a388bed188
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-02-27 01:36:14 +0900

    fix

--- src/eval.py
+++ src/eval.py
@@ -13,13 +13,14 @@ def main(model: str):
   model.load_state_dict(state)
   model.eval()
 
-  tasks, query_set = episoder.get_episode()
-  cnt = 0
-  for feature, label in DataLoader(query_set, shuffle=True):
-    pred = model.forward(feature)
-    if torch.argmax(pred) == torch.argmax(label): cnt += 1
-  # for
-  print(f"accuracy: {cnt / len(query_set):.2f}({cnt}/{len(query_set)})")
+  cnt, iters, n_query_set = 0, 5, episoder.n_query * len(episoder.classes)
+  for _ in range(iters):
+    _, query_set = episoder.get_episode()
+    for feature, label in DataLoader(query_set, shuffle=True):
+      pred = model.forward(feature)
+      if torch.argmax(pred) == torch.argmax(label): cnt += 1
+    # for
+  print(f"accuracy: {cnt / n_query_set * iters:.2f}({cnt}/{n_query_set * iters})")
 # main
 
 if __name__ == "__main__": main(model="./model/model.pth")
--- src/train.py
+++ src/train.py
@@ -4,10 +4,10 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from torch import nn
 from src.model.MAML import MAML
-from src.MAMLDataset import MAMLEpisoder
+from src.MAMLEpisoder import MAMLEpisoder
 import random
 
-def main(path: str, save_to: str, n_way: int, k_shot: int, n_query: int, inner_iters: int, iters: int):
+def main(path: str, save_to: str, n_way: int, k_shot: int, n_query: int, iters: int, epochs: int):
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # init device
 
   imageset = tv.datasets.ImageFolder(root=path)  # load dataset
@@ -19,29 +19,29 @@ def main(path: str, save_to: str, n_way: int, k_shot: int, n_query: int, inner_i
     tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
   ]) # transform
 
-  # set framework
-  inner_lr, lr = 0.001, 0.001
-  n_inpt, h_inpt, n_output = (3, 9, n_way)
+  # set model config and hps
+  alpha, beta = 0.001, 0.001
+  model_config = (3, 9, n_way, (iters, alpha))
 
   # create FSL episode generator
   chosen_classes = random.sample(list(imageset.class_to_idx.values()), n_way)
   episoder = MAMLEpisoder(imageset, chosen_classes, k_shot, n_query, transform)
 
   # init model
-  model = MAML(n_inpt, h_inpt, n_output, inner_lr, inner_iters).to(device)
+  model = MAML(*model_config).to(device)
   criterion = nn.MSELoss()
-  optim = torch.optim.Adam(model.parameters(), lr=lr)
+  optim = torch.optim.Adam(model.parameters(), lr=beta)
 
   # train loop
-  progress_bar, whole_loss = tqdm(range(iters)), float()
+  progress_bar, whole_loss = tqdm(range(epochs)), float()
 
   for _ in progress_bar:
     tasks, query_set = episoder.get_episode()
     fast_adaptions = list()
-    # 이너루프: 개발 작업에 연관되어 지역 매개변수를 갱신합니다.
+    # inner loop
     for task in tasks: fast_adaptions.append(model.inner_update(task, device))
     loss = float()
-    # 아우터 루프: 모든 작업에 연관되어 메타/전역 매개변수를 갱신하여 이를 실제 모델에 반영합니다.
+    # outer loop
     for feature, label in DataLoader(query_set, shuffle=True):
       for fast_adaption in fast_adaptions:
         pred = model._forward(feature, fast_adaption)
@@ -64,11 +64,11 @@ def main(path: str, save_to: str, n_way: int, k_shot: int, n_query: int, inner_i
   features = {
     "state": model.state_dict(),
     "transform": transform,
-    "model config": (n_inpt, h_inpt, n_output),
+    "model config": model_config,
     "episoder": episoder
   }  # features
   torch.save(features, save_to)
   print(f"model save to {save_to}")
 # main
 
-if __name__ == "__main__": main(path="../data/omniglot-py/images_background/Futurama", save_to="./model/model.pth", n_way=5, k_shot=5, n_query=2, inner_iters=20, iters=20)
+if __name__ == "__main__": main(path="../data/omniglot-py/images_background/Futurama", save_to="./model/model.pth", n_way=5, k_shot=5, n_query=2, epochs=5, iters=5)
