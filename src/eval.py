3bbb81dbe0066cc4cbc24cece391420e510ffc5e
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-03-01 22:35:29 +0900

    reform: improve code lines

--- src/eval.py
+++ src/eval.py
@@ -1,8 +1,5 @@
 import random
-from tqdm import tqdm
-from config import HYPERPARAMETER_CONFIG
 import torch
-from torch import nn
 from torch.utils.data import DataLoader
 import torchvision as tv
 from src.FewShotEpisoder import FewShotEpisoder
@@ -16,17 +13,22 @@ def evaluate(MODEL: str, DATASET: str):
   model = MAML(*data["model_config"]).to(device)
   model.load_state_dict(data["state"])
 
-  # create FSL episode generator
+  # overall configuration
   n_way, k_shot, n_query = data["framework"]
-  imageset = tv.datasets.ImageFolder(root=DATASET)  # load dataset
-  seen_classes = data['seen_classes']
-  unseen_classes = random.sample(list(imageset.class_to_idx.values()), n_way)
-  episoder = FewShotEpisoder(imageset, unseen_classes, k_shot, n_query, data["transform"])
 
-  tasks, query_set = episoder.get_episode()
-  adaptions = list()
+  # load imageset
+  transform = data["transform"]  # define transform
+
+  # create FewShotEpisoder which creates tuple of (support set, query set)
+  imageset = tv.datasets.ImageFolder(root=DATASET)
+  seen_classes, unseen_classes = data['seen_classes'], random.sample(list(imageset.class_to_idx.values()), n_way)
+  episoder = FewShotEpisoder(imageset, unseen_classes, k_shot, n_query, transform)
+
+  # fast adaption(inner loop)
+  (tasks, query_set), adaptions = episoder.get_episode(), list()
   for task in tasks: adaptions.append(model.inner_update(task))
 
+  # evaluate
   count, n_problem = 0, len(query_set)
   for feature, label in DataLoader(query_set, shuffle=True):
     task_i = torch.argmax(label).item()
@@ -34,7 +36,6 @@ def evaluate(MODEL: str, DATASET: str):
     if torch.argmax(pred) == torch.argmax(label): count += 1
   # for
   print(f"seen classes: {seen_classes}\nunseen classes: {unseen_classes}\naccuracy: {count / n_problem:.4f}({count}/{n_problem})")
-  # main()
 # main
 
 if __name__ == "__main__": evaluate("./model/model.pth", "../data/omniglot-py/images_background/Futurama")
