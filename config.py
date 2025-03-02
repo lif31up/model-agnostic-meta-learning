8f87902c75481fdcab9e5d76cc5c8d841a85f1fd
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2025-03-03 02:27:35 +0900

    feat: tunned config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 HYPERPARAMETER_CONFIG = {
-  "alpha": 0.01,
-  "beta": 0.001
+  "alpha": 0.001,
+  "beta": 0.0001
 } # HYPERPARAMETER_CONFIG
 TRAINING_CONFIG = {
   "iters": 10,
-  "epochs": 10,
+  "epochs": 50,
 } # TRAINING_CONFIG
 MODEL_CONFIG = {
   "n_inpt": 3,
-  "n_hidn": 9,
+  "n_hidn": 6,
   "n_oupt": 5
 } # MODEL_CONFIG
